970d63f4c156fbde0b34ae3ef35f6b58a977705f
Oliver Hookins <ohookins@users.noreply.github.com>
2021-05-23 21:42:43 +1000

    Remove constraint restrictions

--- terraform-bundle.tf
+++ terraform-bundle.tf
@@ -2,52 +2,52 @@ terraform {
   required_providers {
     archive = {
       source = "hashicorp/archive"
-      version = "~> 2.1"
+      version = ">= 2.1"
     }
 
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.31"
+      version = ">= 3.31"
     }
 
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 2.18"
+      version = ">= 2.18"
     }
 
     cloudinit = {
       source = "hashicorp/cloudinit"
-      version = "~> 2.2"
+      version = ">= 2.2"
     }
 
     datadog = {
       source  = "DataDog/datadog"
-      version = "~> 2.22"
+      version = ">= 2.22"
     }
 
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.2"
+      version = ">= 2.2"
     }
 
     null = {
       source = "hashicorp/null"
-      version = "~> 3.1"
+      version = ">= 3.1"
     }
 
     tls = {
       source = "hashicorp/tls"
-      version = "~> 3.1"
+      version = ">= 3.1"
     }
 
     random = {
       source = "hashicorp/random"
-      version = "~> 3.1"
+      version = ">= 3.1"
     }
 
     vault = {
       source = "hashicorp/vault"
-      version = "~> 2.18"
+      version = ">= 2.18"
     }
   }
 }
